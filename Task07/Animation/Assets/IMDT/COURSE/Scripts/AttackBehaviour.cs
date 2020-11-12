5a297fa4bf8eb460024f898780d73f96acd1b501
Shuopeng98 <lee3014@icloud.com>
2020-11-12 13:32:30 +0800

    Task 07

---
+++ Task07/Animation/Assets/IMDT/COURSE/Scripts/AttackBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBehaviour : GenericBehaviour
+{
+    // Start is called before the first frame update
+    public string hitButton = "Hit";
+    private int hitBool;
+    private int moveBool;
+    private int jumpBool;
+    private Animator anim;
+    private float speed;
+
+    void Start()
+    {
+        hitBool = Animator.StringToHash("Hit");
+        moveBool = Animator.StringToHash("Speed");
+        jumpBool = Animator.StringToHash("Jump");
+        anim = GetComponent<Animator>();
+        behaviourManager.SubscribeBehaviour(this);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        speed = anim.GetFloat(moveBool);
+        bool now = anim.GetBool(hitBool);
+        bool jump = anim.GetBool(jumpBool);
+        if ((speed>0.01f|| jump) && now)
+        {
+            anim.SetBool(hitBool, false);
+        }
+        if (VirtualInput.GetButtonDown(hitButton))
+        {
+            now = anim.GetBool(hitBool);
+            anim.SetBool(hitBool, !now);
+        }
+    }
+}
